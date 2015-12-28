1525555e84e650545369523b8076efc67c4292af
jean-denis courcol <jean-denis.courcol@epfl.ch>
2015-12-28 13:43:28 +0100

    pylint errors

--- fabric/fabfile.py
+++ fabric/fabfile.py
@@ -8,15 +8,18 @@ BWAPI_TAR = ''
 CHAOSLAUNCHER_REG = ''
 SC_TAR = ''
 
+
 def w7_64():
     ''' definition of w7_64 environment '''
     env.user = 'vagrant'
     env.hosts = ['localhost:59857']
 
+
 def _attach_iso(iso_path):
     attach_iso = "VBoxManage storageattach '%s' --storagectl 'IDE Controller' \
-    --port 0 --device 0 --type dvddrive --medium '%s'"%(VM_NAME, iso_path)
+    --port 0 --device 0 --type dvddrive --medium '%s'" % (VM_NAME, iso_path)
     local(attach_iso)
+
 
 def install_visual_studio():
     ''' install visual studio '''
@@ -27,29 +30,35 @@ def install_visual_studio():
 
 def install_bwapi():
     ''' install bwapi '''
-    #takes around 70s
-    #wget https://github.com/bwapi/bwapi/releases/download/v4.1.0-Beta/BWAPI_410B_2_Setup.exe
+    # takes around 70s
+    # wget https://github.com/bwapi/bwapi/releases/download/v4.1.0-Beta/BWAPI_410B_2_Setup.exe
     with cd('/home/vagrant/'):
         put('', '/home/vagrant', mode=0755)
-        #run('icacls %s /grant Administrator:F' % f)
+        # run('icacls %s /grant Administrator:F' % f)
         put('chaoslauncher.reg',
             '/home/vagrant/chaoslauncher.reg',
             mode=0755)
-        #cf superuser 664756
-        run('cmd /c "%SystemRoot%\\regedit.exe /s `cygpath -w /home/vagrant/chaoslauncher.reg`"')
+        # cf superuser 664756
+        run('cmd /c "%SystemRoot%\\regedit.exe /s \
+        `cygpath -w /home/vagrant/chaoslauncher.reg`"')
+
 
 def install_starcraft():
     ''' run the starcraft installation '''
     # takes around 60s
-    #may fail if never logged in as vagrant to the remote with cygwin console.
+    # may fail if never logged in as vagrant to the remote with cygwin console.
     put("starcraft", "/home/vagrant/", mode=0755)
     run('./SETUP.EXE ', timeout=10, quiet=True)
+
 
 def build_example_bot():
     ''' build the example bot provided with BWAPI '''
     bot_project = "/home/vagrant/BWAPI/ExampleAIModule/ExampleAIModule.vcxproj"
-    msbuild = "\"/cygdrive/c/Program Files (x86)/MSBuild/12.0/Bin/MSBuild.exe\""
-    run('%s `cygpath -aw %s` /p:Configuration=Release /p:Platform=Win32' % (msbuild, bot_project))
+    msbuild = "\"/cygdrive/c/Program Files\
+    (x86)/MSBuild/12.0/Bin/MSBuild.exe\""
+    run('%s `cygpath -aw %s` /p:Configuration=Release\
+    /p:Platform=Win32' % (msbuild, bot_project))
+
 
 def deploy():
     ''' function called by fabric '''
